d4ddca3d816705abdf3df0507a81ed03598af3a8
vaisakh.cp@ima360.com <121595062+vaisakhChandranP@users.noreply.github.com>
2023-09-28 03:45:58 +0530

    added profile page

--- App.js
+++ App.js
@@ -1,134 +1,102 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useRef } from "react";
+import { Button, StyleSheet, Text, View, Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Amplify } from "aws-amplify";
 import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Pressable,
-  SafeAreaView,
-} from 'react-native';
-import {API, graphqlOperation} from 'aws-amplify';
-import {createTodo} from './src/graphql/mutations';
-import {listTodos} from './src/graphql/queries';
-import {
-  withAuthenticator,
+  Authenticator,
   useAuthenticator,
-} from '@aws-amplify/ui-react-native';
-import { Amplify } from 'aws-amplify';
-import awsExports from './src/aws-exports';
-Amplify.configure(awsExports);
+  useTheme,
+} from "@aws-amplify/ui-react-native";
+import awsconfig from "./src/aws-exports";
+import ProfilePage from "./src/Pages/ProfilePage";
+import { Auth } from "aws-amplify";
 
-// retrieves only the current value of 'user' from 'useAuthenticator'
-const userSelector = (context) => [context.user]
+Amplify.configure(awsconfig);
 
-const SignOutButton = () => {
-  const { user, signOut } = useAuthenticator(userSelector);
-  return (
-    <Pressable onPress={signOut} style={styles.buttonContainer}>
-      <Text style={styles.buttonText}>
-r      </Text>
-    </Pressable>
-  );
-};
-
-const initialFormState = {name: 'kochan', dateOfBirth: '1970-01-01Z',country:'India',state:'kerala'};
-
-const App = () => {
-  const [formState, setFormState] = useState(initialFormState);
-  const [todos, setTodos] = useState([]);
-  const [date, setDate] = useState(new Date()); 
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-
-  function setInput(key, value) {
-    setFormState({...formState, [key]: value});
-  }
-  const onChange = (event, selectedDate) => {
-    setShow(Platform.OS === 'ios');
-    setDate(selectedDate || date);
-  };
-
-  async function fetchTodos() {
-    try {
-      const todoData = await API.graphql(graphqlOperation(listTodos));
-      const todos = todoData.data.listTodos.items;
-      setTodos(todos);
-    } catch (err) {
-      console.log('error fetching todos');
-    }
-  }
-
-  async function addTodo() {
-    console.log('step 1')
-    try {
-      if (!formState.name || !formState.state) return;
-      const todo = {...formState};
-      setTodos([...todos, todo]);
-      setFormState(initialFormState);
-      await API.graphql(graphqlOperation(createTodo, {input: todo}));
-    } catch (err) {
-      console.log('error creating todo:', err);
-    }
-  }
-  useEffect(() => {
-    console.log(formState)
-  },[formState])
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <SignOutButton />
-        <TextInput
-          onChangeText={value => setInput('name', value)}
-          style={styles.input}
-          value={formState.name}
-          placeholder="Name"
-        />
-        <TextInput
-          onChangeText={value => setInput('description', value)}
-          style={styles.input}
-          value={formState.dateOfBirth}
-          placeholder="Description"
-        />
-          <TextInput
-          onChangeText={value => setInput('dateOfBirth', value)}
-          style={styles.input}
-          value={formState.dateOfBirth}
-          placeholder="DOB"
-        />
-         <TextInput
-          onChangeText={value => setInput('country', value)}
-          style={styles.input}
-          value={formState.country}
-          place holder="Country"
-        />
-        <Pressable onPress={addTodo} style={styles.buttonContainer}>
-          <Text style={styles.buttonText}>Create todo</Text>
-        </Pressable>
-      
-        {todos.map((todo, index) => (
-          <View key={todo.id ? todo.id : index} style={styles.todo}>
-            <Text style={styles.todoName}>{todo.name}</Text>
-            <Text style={styles.todoDescription}>{todo.description}</Text>
-          </View>
-        ))}
-      </View>
-    </SafeAreaView>
-  );
-};
-
-export default withAuthenticator(App);
+const Stack = createStackNavigator();
 
 const styles = StyleSheet.create({
-  container: {width: 400, flex: 1, padding: 20, alignSelf: 'center'},
-  todo: {marginBottom: 15},
-  input: {backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18},
-  todoName: {fontSize: 20, fontWeight: 'bold'},
-  buttonContainer: {
-    alignSelf: 'center',
-    backgroundColor: 'black',
-    paddingHorizontal: 8,
+  logo: {
+    width: 150,
+    resizeMode: "contain",
+    marginBottom: 20,
   },
-  buttonText: {color: 'white', padding: 16, fontSize: 18},
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+  },
 });
+
+const MyAppHeader = () => {
+  const {
+    tokens: { space, fontSizes },
+  } = useTheme();
+  return (
+    <View style={styles.container}>
+      <Image source={require("./assets/logo.png")} style={styles.logo} />
+    </View>
+  );
+};
+
+function SignOutButton() {
+  const { signOut } = useAuthenticator();
+  return <Button onPress={signOut} title="Sign Out" />;
+}
+
+// function HomePage({ navigation }) {
+//   return (
+//     <View style={style.container}>
+//       <Text>Home Screen</Text>
+//       <Button
+//         title="Go to Profile Page"
+//         onPress={() => navigation.navigate("ProfilePage")}
+//       />
+//       <SignOutButton />
+//     </View>
+//   );
+// }
+
+// function ProfilePage() {
+//   return (
+//     <View style={style.container}>
+//       <Text>This is the Profile Page!</Text>
+//     </View>
+//   );
+// }
+
+function App() {
+  const navigationRef = useRef(null);
+
+  useEffect(() => {
+    async function checkAuthStatus() {
+      try {
+        await Auth.currentAuthenticatedUser();
+        navigationRef.current?.navigate("ProfilePage");
+      } catch (error) {
+        console.log("User is not authenticated", error);
+      }
+    }
+
+    checkAuthStatus();
+  }, []);
+
+  return (
+    <Authenticator.Provider>
+      <Authenticator Header={MyAppHeader}>
+        <NavigationContainer ref={navigationRef}>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="ProfilePage" component={ProfilePage} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Authenticator>
+    </Authenticator.Provider>
+  );
+}
+
+const style = StyleSheet.create({
+  container: { flex: 1, alignItems: "center", justifyContent: "center" },
+});
+
+export default App;
---
+++ src/Pages/ProfilePage.js
@@ -0,0 +1,131 @@
+// ProfilePage.js
+import React, {useEffect, useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  SafeAreaView,
+} from 'react-native';
+import {API, graphqlOperation} from 'aws-amplify';
+import {createTodo} from '../../src/graphql/mutations';
+import {listTodos} from '../../src/graphql/queries';
+import {
+  withAuthenticator,
+  useAuthenticator,
+} from '@aws-amplify/ui-react-native';
+import { Amplify } from 'aws-amplify';
+
+const initialFormState = {name: 'kochan', dateOfBirth: '1970-01-01Z',country:'India',state:'kerala'};
+const userSelector = (context) => [context.user]
+
+function ProfilePage() {
+    const [formState, setFormState] = useState(initialFormState);
+  const [todos, setTodos] = useState([]);
+  const [date, setDate] = useState(new Date()); 
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+const SignOutButton = () => {
+  const { user, signOut } = useAuthenticator(userSelector);
+  return (
+    <Pressable onPress={signOut} style={styles.buttonContainer}>
+      <Text style={styles.buttonText}>
+      </Text>
+    </Pressable>
+  );
+};
+
+  function setInput(key, value) {
+    setFormState({...formState, [key]: value});
+  }
+  const onChange = (event, selectedDate) => {
+    setShow(Platform.OS === 'ios');
+    setDate(selectedDate || date);
+  };
+
+  async function fetchTodos() {
+    try {
+      const todoData = await API.graphql(graphqlOperation(listTodos));
+      const todos = todoData.data.listTodos.items;
+      setTodos(todos);
+    } catch (err) {
+      console.log('error fetching todos');
+    }
+  }
+
+  async function addTodo() {
+    console.log('step 1')
+    try {
+      if (!formState.name || !formState.state) return;
+      const todo = {...formState};
+      setTodos([...todos, todo]);
+      setFormState(initialFormState);
+      await API.graphql(graphqlOperation(createTodo, {input: todo}));
+    } catch (err) {
+      console.log('error creating todo:', err);
+    }
+  }
+  useEffect(() => {
+    console.log(formState)
+  },[formState])
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <SignOutButton />
+        <TextInput
+          onChangeText={value => setInput('name', value)}
+          style={styles.input}
+          value={formState.name}
+          placeholder="Name"
+        />
+        <TextInput
+          onChangeText={value => setInput('description', value)}
+          style={styles.input}
+          value={formState.dateOfBirth}
+          placeholder="Description"
+        />
+          <TextInput
+          onChangeText={value => setInput('dateOfBirth', value)}
+          style={styles.input}
+          value={formState.dateOfBirth}
+          placeholder="DOB"
+        />
+         <TextInput
+          onChangeText={value => setInput('country', value)}
+          style={styles.input}
+          value={formState.country}
+          place holder="Country"
+        />
+        <Pressable onPress={addTodo} style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>Create todo</Text>
+        </Pressable>
+      
+        {todos.map((todo, index) => (
+          <View key={todo.id ? todo.id : index} style={styles.todo}>
+            <Text style={styles.todoName}>{todo.name}</Text>
+            <Text style={styles.todoDescription}>{todo.description}</Text>
+          </View>
+        ))}
+      </View>
+    </SafeAreaView>
+
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {width: 400, flex: 1, padding: 20, alignSelf: 'center'},
+  todo: {marginBottom: 15},
+  input: {backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18},
+  todoName: {fontSize: 20, fontWeight: 'bold'},
+  buttonContainer: {
+    alignSelf: 'center',
+    backgroundColor: 'black',
+    paddingHorizontal: 8,
+  },
+  buttonText: {color: 'white', padding: 16, fontSize: 18},
+});
+export default ProfilePage;
